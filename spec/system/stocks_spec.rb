691412948d00bab207d0058fb0898351abe78730
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-03 20:22:28 +0900

    失敗パターンのテスト追加

--- spec/system/stocks_spec.rb
+++ spec/system/stocks_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Stocks", type: :system do
+RSpec.describe 'Stocks', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  describe "新しいストック" do
-    context "入力が正しければ" do
-      it "Stock を投稿できる" do
+  describe '新しいストック' do
+    context '入力が正しければ' do
+      it 'Stock を投稿できる' do
         visit '/'
 
         click_on 'Stock!'
@@ -23,6 +23,27 @@ RSpec.describe "Stocks", type: :system do
         expect(page).to have_text('successfully') # TODO: I18n
         expect(page).to have_text('Stock')
         expect(page).to have_text('M78星雲')
+      end
+    end
+
+    context '入力が間違えていれば' do
+      it 'Stock の投稿に失敗し投稿フォームが再表示される' do
+        visit '/'
+
+        click_on 'Stock!'
+
+        fill_in '名称', with: ''
+        fill_in '住所', with: '宇宙の彼方'
+
+        fill_in '訪問日', with: '2020/01/01'
+        choose 'あり'
+
+        click_on 'Save' # TODO: I18n
+
+        expect(page).not_to have_text('successfully') # TODO: I18n
+        expect(page).to have_text('New stock')
+        expect(page).to have_text('名称を入力してください')
+        expect(page).to have_text('宇宙の彼方')
       end
     end
   end
