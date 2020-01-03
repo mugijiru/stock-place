450c72d3826c664d2ccefb4d91a8a4e88a4fef98
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-03 12:08:06 +0900

    request spec を充実させた

--- spec/requests/stocks_spec.rb
+++ spec/requests/stocks_spec.rb
@@ -1,11 +1,70 @@
 require 'rails_helper'
 
-RSpec.describe "Stocks", type: :request do
-  describe "GET /stocks" do
-    it "ストックした場所一覧を取得できる" do
-      create(:place, name: "おいしいラーメン屋")
+RSpec.describe 'Stocks', type: :request do
+  describe 'GET /stocks' do
+    it 'ストックした場所一覧を取得できる' do
+      params = valid_params.merge(name: 'おいしいラーメン屋')
+      create(:place, params)
       get stocks_path
-      expect(response.body).to include("おいしいラーメン屋")
+      expect(response.body).to include('おいしいラーメン屋')
     end
+  end
+
+  describe 'POST /stocks' do
+    it '場所をストックできる' do
+      params = { place: valid_params.merge(address: 'test 1-1-1') }
+      post stocks_path, params: params
+
+      aggregate_failures do
+        expect(response).to have_http_status(:found)
+
+        follow_redirect!
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include('test 1-1-1')
+      end
+    end
+  end
+
+  describe 'PUT /stocks/:id' do
+    it 'ストックした場所を更新できる' do
+      create_params = valid_params.merge(name: 'おいしいラーメン屋')
+      place = create(:place, create_params)
+
+      params = { place: valid_params.merge(name: 'おいしい中華料理屋') }
+      put stock_path(place), params: params
+
+      aggregate_failures do
+        expect(response).to have_http_status(:found)
+
+        follow_redirect!
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).not_to include('ラーメン屋')
+        expect(response.body).to include('中華料理屋')
+      end
+    end
+  end
+
+  describe 'DELETE /stocks/:id' do
+    it 'ストックした場所を削除できる' do
+      create_params = valid_params.merge(name: 'おいしいラーメン屋')
+      place = create(:place, create_params)
+
+      delete stock_path(place)
+
+      aggregate_failures do
+        expect(response).to have_http_status(:found)
+
+        follow_redirect!
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).not_to include('ラーメン屋')
+      end
+    end
+  end
+
+  def valid_params
+    { name: 'test', address: 'test x-y-z' }
   end
 end
