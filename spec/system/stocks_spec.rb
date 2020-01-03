451efd2d2f69fc5f09846598561c5624a38a6f53
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-03 20:10:28 +0900

    既存のテストを describe, context に閉じ込めた
    失敗パターンのテストも追加するため

--- spec/system/stocks_spec.rb
+++ spec/system/stocks_spec.rb
@@ -5,21 +5,25 @@ RSpec.describe "Stocks", type: :system do
     driven_by(:rack_test)
   end
 
-  it "Stock を投稿できる" do
-    visit '/'
+  describe "新しいストック" do
+    context "入力が正しければ" do
+      it "Stock を投稿できる" do
+        visit '/'
 
-    click_on 'Stock!'
+        click_on 'Stock!'
 
-    fill_in '名称', with: 'M78星雲'
-    fill_in '住所', with: '宇宙の彼方'
+        fill_in '名称', with: 'M78星雲'
+        fill_in '住所', with: '宇宙の彼方'
 
-    fill_in '訪問日', with: '2020/01/01'
-    choose 'あり'
+        fill_in '訪問日', with: '2020/01/01'
+        choose 'あり'
 
-    click_on 'Save' # TODO: I18n
+        click_on 'Save' # TODO: I18n
 
-    expect(page).to have_text('successfully') # TODO: I18n
-    expect(page).to have_text('Stock')
-    expect(page).to have_text('M78星雲')
+        expect(page).to have_text('successfully') # TODO: I18n
+        expect(page).to have_text('Stock')
+        expect(page).to have_text('M78星雲')
+      end
+    end
   end
 end
