8cf7d678795a1ceb2ab66b8caf9f385e6e00fe7f
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-03 17:54:53 +0900

    Enumerize でデータを登録できるようにした

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ApplicationRecord
-  has_many :evaluations, class_name: 'PlaceEvaluation'
+  has_many :evaluations, class_name: 'PlaceEvaluation', inverse_of: :place
 
   validates :name, presence: true
 end
--- app/models/place_evaluation.rb
+++ app/models/place_evaluation.rb
@@ -1,6 +1,10 @@
 class PlaceEvaluation < ApplicationRecord
+  extend Enumerize
+
   belongs_to :place
 
   validates :visited_on, presence: true
   validates :point, presence: true, numericality: { only_integer: true }
+
+  enumerize :point, in: { no_good: -1, no_comment: 0, not_bad: 1, good: 2 }
 end
