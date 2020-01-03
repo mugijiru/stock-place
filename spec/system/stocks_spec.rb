55246844584dc16f87b7691d0b32b6413e3a12df
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-03 19:26:59 +0900

    期待する挙動を system spec に記述

--- spec/system/stocks_spec.rb
+++ spec/system/stocks_spec.rb
@@ -5,5 +5,21 @@ RSpec.describe "Stocks", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it "Stock を投稿できる" do
+    visit '/'
+
+    click_on 'Stock!'
+
+    fill_in '名称', with: 'M78星雲'
+    fill_in '住所', with: '宇宙の彼方'
+
+    fill_in '訪問日', with: '2020/01/01'
+    choose 'あり'
+
+    click_on 'Save' # TODO: I18n
+
+    expect(page).to have_text('Successfly') # TODO: I18n
+    expect(page).to have_text('Stock')
+    expect(page).to have_text('M78星雲')
+  end
 end
