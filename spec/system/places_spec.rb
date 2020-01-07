d6c07d1b877b3388ef6b4272465a0e0ab6731fff
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-07 09:06:10 +0900

    更新処理の system spec も追加

---
+++ spec/system/places_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Places", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe '場所情報の更新' do
+    context '入力が正しければ' do
+      it '場所情報が更新できる' do
+        place = create(:place, name: 'old_name', address: 'old_address')
+        visit "/places/#{place.id}"
+        click_on('編集')
+        fill_in('名称', with: 'new_name')
+        fill_in('住所', with: 'new_address')
+        click_on('Save')
+        expect(page).to have_content('successfully')
+        expect(page).not_to have_content('old_name')
+        expect(page).not_to have_content('old_address')
+        expect(page).to have_content('new_name')
+        expect(page).to have_content('new_address')
+      end
+    end
+  end
+end
