66724d6ade2faca9556b200736911bd4a2ce22a3
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-08 12:14:08 +0900

    view を追加してテストをある程度実行できるようにした

--- app/controllers/places/evaluations_controller.rb
+++ app/controllers/places/evaluations_controller.rb
@@ -1,8 +1,14 @@
 class Places::EvaluationsController < ApplicationController
+  def new
+    @place = Place.find(params[:place_id])
+    @evaluation = @place.evaluations.build
+  end
+
   def create
     @place = Place.find(params[:place_id])
+    @evaluation = @place.evaluations.build(evaluation_params)
 
-    if @place.evaluations.create(evaluation_params)
+    if @evaluation.save
       redirect_to place_path(@place), notice: 'Evaluation was successfully created.'
     else
       render :new
