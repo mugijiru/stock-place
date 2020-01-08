f06bba99734ec3189009a174f2569513faca0cee
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-08 09:54:48 +0900

    それっぽい controller を用意した。view がないのでテストは通らない

--- app/controllers/places/evaluations_controller.rb
+++ app/controllers/places/evaluations_controller.rb
@@ -1,3 +1,17 @@
 class Places::EvaluationsController < ApplicationController
+  def create
+    @place = Place.find(params[:place_id])
 
+    if @place.evaluations.create(evaluation_params)
+      redirect_to place_path(@place), notice: 'Evaluation was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def evaluation_params
+    params.require(:evaluation).permit(:visited_on, :point)
+  end
 end
