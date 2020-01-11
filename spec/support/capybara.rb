884573bef55f7fa8794bf97eb662a18004517291
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-11 16:12:23 +0900

    特定のテストだけ selenium_chrome_headless でテストできるようにした

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,17 @@
 require 'capybara/rspec'
+
+Capybara.javascript_driver = :selenium_chrome_headless
+
+RSpec.configure do |config|
+  config.before :each do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+      elsif example.metadata[:driver].present?
+        driven_by example.metadata[:driver]
+      else
+        driven_by :rack_test
+      end
+    end
+  end
+end
--- spec/system/places/evaluations_spec.rb
+++ spec/system/places/evaluations_spec.rb
@@ -1,10 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Places::Evaluations", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
   describe '評価の登録' do
     context '入力が正しければ' do
       it '評価を登録できる' do
@@ -47,7 +43,7 @@ RSpec.describe "Places::Evaluations", type: :system do
 
   describe '評価の削除' do
     context '入力が正しければ' do
-      it '評価を削除できる' do
+      it '評価を削除できる', js: true do
         place = create(:place, name: 'test_place')
         evaluation = create(:place_evaluation, place: place, visited_on: '2020/01/01', point: :good)
         visit "/places/#{place.id}"
