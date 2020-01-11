b6e1959e51a8725ec29e6c4736ea2039aaf5ec0f
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-12 00:13:54 +0900

    最終訪問日でソートできるようにした

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,9 @@ class Place < ApplicationRecord
   has_many :evaluations, class_name: 'PlaceEvaluation', inverse_of: :place
 
   validates :name, presence: true
+
+  def self.recently_visited
+    joins('LEFT JOIN place_evaluations ON places.id = place_evaluations.place_id').
+      group('places.id').select('places.*', "COALESCE(MAX(place_evaluations.visited_on), '1900-01-01') AS last_visited_on").order(last_visited_on: :desc)
+  end
 end
