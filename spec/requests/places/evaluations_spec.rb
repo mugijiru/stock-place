7be0898ded666931646b0285a12e86129781e6aa
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-11 13:29:00 +0900

    訪問記録修正の request spec も追加

--- spec/requests/places/evaluations_spec.rb
+++ spec/requests/places/evaluations_spec.rb
@@ -29,4 +29,37 @@ RSpec.describe 'Places::Evaluations', type: :request do
       end
     end
   end
+
+  describe 'PUT /places/:place_id/evaluations/:id' do
+    context '正しいリクエストの場合' do
+      it '評価を修正できる' do
+        place = create(:place, name: 'test_place')
+        evaluation = create(:place_evaluation, place: place, visited_on: Date.yesterday, point: :no_good)
+        put "/places/#{place.id}/evaluations/#{evaluation.id}", params: { place_evaluation: { visited_on: Date.today, point: :good } }
+        aggregate_failures do
+          expect(response).to have_http_status(:found)
+          follow_redirect!
+          expect(response.body).to include('test_place')
+          expect(response.body).to include(Date.today.strftime('%Y/%m/%d'))
+          expect(response.body).to include('また行きたい')
+        end
+      end
+    end
+
+    context '誤ったリクエストの場合' do
+      it '評価を修正できない' do
+        place = create(:place, name: 'test_place')
+        evaluation = create(:place_evaluation, place: place, visited_on: Date.yesterday, point: :bad)
+        put "/places/#{place.id}/evaluations/#{evaluation.id}", params: { place_evaluation: { visited_on: '2020/01/98', point: 2000 } }
+        aggregate_failures do
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to include('test_place')
+          expect(response.body).to include(Date.yesterday.strftime('%Y/%m/%d'))
+          expect(response.body).to include('もう行きたくない')
+          expect(response.body).not_to include(Date.today.strftime('%Y/%m/%d'))
+          expect(response.body).not_to include('また行きたい')
+        end
+      end
+    end
+  end
 end
