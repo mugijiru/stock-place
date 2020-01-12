57ebd13b4d879c247801e1986ec5f6ba6e066e32
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-12 09:51:14 +0900

    Home に表示する項目を実装した

--- app/models/place_evaluation.rb
+++ app/models/place_evaluation.rb
@@ -6,5 +6,5 @@ class PlaceEvaluation < ApplicationRecord
   validates :visited_on, presence: true
   validates :point, presence: true, numericality: { only_integer: true }
 
-  enumerize :point, in: { no_good: -1, no_comment: 0, not_bad: 1, good: 2 }
+  enumerize :point, in: { no_good: -1, no_comment: 0, not_bad: 1, good: 2 }, scope: :having_point
 end
