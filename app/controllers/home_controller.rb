4e9cd9b5d3a25bdfcfe38f529dd6438bf1f28615
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-12 10:17:09 +0900

    Bullet 様の教えに従い N+1 を修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     @grouped_evaluations = {
-      good: PlaceEvaluation.having_point(:good).order(id: :desc).limit(10),
-      not_bad: PlaceEvaluation.having_point(:not_bad).order(id: :desc).limit(10),
+      good: PlaceEvaluation.having_point(:good).eager_load(:place).order(id: :desc).limit(10),
+      not_bad: PlaceEvaluation.having_point(:not_bad).eager_load(:place).order(id: :desc).limit(10),
     }
   end
 end
