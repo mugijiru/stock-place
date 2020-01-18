433c5960dc00f92df319d34457f2181f780d0f98
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-18 23:01:56 +0900

    new-stock で vuex が使えるようにする初期設定

--- app/javascript/packs/new-stock.js
+++ app/javascript/packs/new-stock.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import App from '../vue/new-stock/application.vue'
 import router from '../vue/new-stock/routes'
 
+Vue.use(Vuex)
 Vue.use(VueRouter)
 
 document.addEventListener('DOMContentLoaded', () => {
