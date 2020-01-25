fa5894f3bf827b575a8eb5e1f0cb731897ea90cc
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-25 23:01:57 +0900

    場所登録用の API を実装

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,6 +1,25 @@
-class Api::V1::PlacesController < ApplicationController
-  def index
-    @places = Place.all
-    render json: @places.to_json
+module Api
+  module V1
+    class PlacesController < ApplicationController
+      def index
+        @places = Place.all
+        render json: @places.to_json
+      end
+
+      def create
+        @place = Place.new(place_params)
+
+        @place.save!
+        render json: @place, status: :created
+      rescue ActiveRecord::RecordInvalid
+        render json: { place: @place, errors: @place.errors.full_messages }, status: :unprocessable_entity
+      end
+
+      private
+
+      def place_params
+        params.require(:place).permit(:name, :address)
+      end
+    end
   end
 end
