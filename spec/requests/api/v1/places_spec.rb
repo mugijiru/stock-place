69a1b40ab4b9d90e2a1f028a3269749306d06e44
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-25 22:29:12 +0900

    API を生やしたいので先にテストを用意

--- spec/requests/api/v1/places_spec.rb
+++ spec/requests/api/v1/places_spec.rb
@@ -15,4 +15,32 @@ RSpec.describe 'Api::V1::PlacesController', type: :request do
       end
     end
   end
+
+  describe 'POST /api/v1/places' do
+    context 'パラメータが正しければ' do
+      it '場所を新規登録できる' do
+        post '/api/v1/places', params: { place: { name: 'M78星雲', address: '宇宙の彼方' } }
+
+        aggregate_failures do
+          expect(response).to have_http_status(:created)
+
+          json = JSON.parse(response.body)
+          expect(json['name']).to eq('M78星雲')
+        end
+      end
+    end
+
+    context 'パラメータが誤っていれば' do
+      it '場所の登録に失敗' do
+        post '/api/v1/places', params: { place: { name: '', address: '宇宙の彼方' } }
+
+        aggregate_failures do
+          expect(response).to have_http_status(:unprocessable_entity)
+
+          json = JSON.parse(response.body)
+          expect(json['errors']).to include('名称を入力してください')
+        end
+      end
+    end
+  end
 end
