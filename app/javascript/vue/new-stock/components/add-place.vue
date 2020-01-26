d4421972ff3b415808f967265240283c3e9ae5db
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-26 16:40:21 +0900

    axios に CSRF token を設定する記述を1箇所にまとめた

--- app/javascript/vue/new-stock/components/add-place.vue
+++ app/javascript/vue/new-stock/components/add-place.vue
@@ -13,10 +13,7 @@
 </template>
 
 <script>
-import axios from 'axios';
-if (process.env.RAILS_ENV !== 'test') {
-  axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('[name="csrf-token"]').getAttribute('content')
-}
+import axios from 'libs/axios';
 
 import { mapActions } from 'vuex'
 
