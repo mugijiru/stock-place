19f89e0795e1424b2b6e61cac88682d29aba5109
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-01-26 22:18:13 +0900

    場所選択画面のリストをそれっぽくした

--- app/javascript/vue/new-stock/components/select-place.vue
+++ app/javascript/vue/new-stock/components/select-place.vue
@@ -1,10 +1,14 @@
 <template lang='pug'>
 section
-  h3 場所はどこ?
-  button(@click='$router.push({ path: "/add-place" })') 場所を追加
-  ul
-    li(v-for='place in sortedPlaces' :key='place.id')
-      button(@click='set(place.id)') {{ place.name }}
+  .p-content-header
+    h3.p-content-header__title 場所はどこ?
+    button.c-button(@click='$router.push({ path: "/add-place" })') 新規登録
+
+  .p-selectable-places
+    .p-selectable-places__header この中にある?
+    ul.p-selectable-places__list
+      li.p-selectable-places__item(v-for='place in sortedPlaces' :key='place.id')
+        button.p-selectable-places__select-button(@click='set(place.id)') {{ place.name }}
 </template>
 
 <script>
@@ -32,4 +36,32 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.p-selectable-places {
+  margin-top: 20px;
+
+  &__header {
+    width: 100%;
+    height: 40px;
+    padding-left: 10px;
+    line-height: 40px;
+    color: #fff;
+    background-color: #5288bc;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+  }
+
+  &__item {
+    height: 40px;
+    font-size: 13px;
+    font-weight: bold;
+    background: linear-gradient(180deg, #ffffff 0%, rgba(255, 255, 255, 0) 100%), #f6f6f6;
+  }
+
+  &__select-button {
+    width: 100%;
+    height: 100%;
+    padding-left: 10px;
+    text-align: left;
+  }
+}
 </style>
