3a083a9319df8cd84bd561cfb2f0b1100fcbe633
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-02-02 17:34:29 +0900

    未来の日付では保存されないようにした

--- app/models/place_evaluation.rb
+++ app/models/place_evaluation.rb
@@ -16,7 +16,16 @@ class PlaceEvaluation < ApplicationRecord
   belongs_to :place
 
   validates :visited_on, presence: true
+  validate :validate_today_or_past_date
   validates :point, presence: true, numericality: { only_integer: true }
 
   enumerize :point, in: { no_good: -1, no_comment: 0, not_bad: 1, good: 2 }, scope: :having_point
+
+  private
+
+  def validate_today_or_past_date
+    return unless visited_on.present?
+
+    errors.add(:visited_on, '未来の日付を選択することはできません') if visited_on > Date.today
+  end
 end
