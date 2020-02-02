88d1eeb8c68a3829f27779890dda732f19f11bdd
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-02-02 18:41:04 +0900

    テスト内容と記述がズレていたので修正

--- spec/requests/api/v1/place_evaluations_spec.rb
+++ spec/requests/api/v1/place_evaluations_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe 'Api::V1::PlaceEvaluationsController', type: :request do
   describe 'POST /api/v1/place_evaluations' do
-    it '登録済の場所一覧を取得できる' do
+    it '場所と評価を保存できる' do
       place = create(:place, name: 'test place')
 
       post '/api/v1/place_evaluations', params: { place_evaluation: { place_id: place.id, visited_on: Date.today, point: :good }}
