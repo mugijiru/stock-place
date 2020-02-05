1d9b82bff3b64c5cf0de85ab1d7c05525e85f25a
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-02-05 23:40:10 +0900

    spec のパラメータ名も新カラムに合わせて修正

--- spec/requests/api/v1/place_evaluations_spec.rb
+++ spec/requests/api/v1/place_evaluations_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Api::V1::PlaceEvaluationsController', type: :request do
       it '場所と評価を保存できる' do
         place = create(:visited_place, name: 'test place')
 
-        post '/api/v1/place_evaluations', params: { place_evaluation: { place_id: place.id, visited_on: Date.today, evaluation: :good }}
+        post '/api/v1/place_evaluations', params: { place_evaluation: { visited_place_id: place.id, visited_on: Date.today, evaluation: :good }}
 
         aggregate_failures do
           expect(response).to have_http_status(:created)
@@ -21,7 +21,7 @@ RSpec.describe 'Api::V1::PlaceEvaluationsController', type: :request do
       it '場所と評価を保存できない' do
         place = create(:visited_place, name: 'test place')
 
-        post '/api/v1/place_evaluations', params: { place_evaluation: { place_id: place.id, visited_on: Date.tomorrow, evaluation: :good }}
+        post '/api/v1/place_evaluations', params: { place_evaluation: { visited_place_id: place.id, visited_on: Date.tomorrow, evaluation: :good }}
 
         aggregate_failures do
           expect(response).to have_http_status(:unprocessable_entity)
