878a7fbccf565850e4f6be4d1d10768ad67cb2af
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-03-08 21:09:12 +0900

    vue-thin-modal の初期設定追加

--- app/javascript/packs/new-stock.js
+++ app/javascript/packs/new-stock.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueThinModal from 'vue-thin-modal'
 import App from '../vue/new-stock/application.vue'
 import store from '../vue/new-stock/store'
 
 Vue.use(Vuex)
+Vue.use(VueThinModal)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
