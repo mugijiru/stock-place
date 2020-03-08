5696a9cf96710141d7c8bf6a47987f62219aa937
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-03-08 22:03:16 +0900

    成立条件が難しくて pending にしていたテストを削除
    成立条件が難しくいつまでも pending のままなので

--- spec/system/stocks_spec.rb
+++ spec/system/stocks_spec.rb
@@ -48,25 +48,6 @@ RSpec.describe 'Stocks', type: :system, js: true do
           expect(page).to have_text('M78星雲')
         end
       end
-
-      context '入力が間違えていれば' do
-        xit 'Stock の投稿に失敗し投稿フォームが再表示される' do
-          visit '/'
-
-          click_on 'Stock!'
-
-          click_on 'M78星雲'
-          # NOTE: Chrome が6桁西暦に対応しているので0埋めで調整
-          fill_in 'place_evaluation_visited_on', with: '002020/01/01'
-          choose 'また行きたい'
-
-          click_on '保存'
-
-          expect(page).to have_text('New stock')
-          expect(page).to have_text('名称を入力してください')
-          expect(page).to have_field('住所', with: '宇宙の彼方')
-        end
-      end
     end
   end
 end
