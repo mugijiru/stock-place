a3ea246de546e09708b4005c7e97c33c657f02ab
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-03-14 15:19:32 +0900

    Controller 周りも調整

--- spec/requests/visited_places_spec.rb
+++ spec/requests/visited_places_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe 'Places', type: :request do
-  describe 'GET /places/:id' do
+RSpec.describe 'VisitedPlaces', type: :request do
+  describe 'GET /visited_places/:id' do
     it '場所情報と評価一覧が表示される' do
       place = create(:visited_place, name: 'for places test')
       dates = [Date.today, Date.today - 3.days, Date.today - 7.days]
@@ -9,7 +9,7 @@ RSpec.describe 'Places', type: :request do
         create(:visited_place_report, visited_place: place, visited_on: date)
       end
 
-      get "/places/#{place.id}"
+      get "/visited_places/#{place.id}"
 
       aggregate_failures do
         expect(response).to have_http_status(:success)
@@ -21,10 +21,10 @@ RSpec.describe 'Places', type: :request do
     end
   end
 
-  describe 'PUT /places/:id' do
+  describe 'PUT /visited_places/:id' do
     it '場所情報が更新できる' do
       place = create(:visited_place, name: 'for update', address: 'foo')
-      put "/places/#{place.id}", params: { visited_place: { name: 'updated_name', address: 'bar' } }
+      put "/visited_places/#{place.id}", params: { visited_place: { name: 'updated_name', address: 'bar' } }
       aggregate_failures do
         expect(response).to have_http_status(:found)
         follow_redirect!
