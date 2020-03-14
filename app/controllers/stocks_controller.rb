a72a0c78fc46b02886d2728b7a02ed5567f63d5b
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-03-14 14:30:05 +0900

    API 以外では JSON は期待しないので respond_to を使わないようにした

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -17,14 +17,10 @@ class StocksController < ApplicationController
     @place = VisitedPlace.new(place_params)
     @place_evaluation = @place.reports.build(place_evaluation_params)
 
-    respond_to do |format|
-      if @place.save
-        format.html { redirect_to place_path(@place), notice: 'VisitedPlace was successfully created.' }
-        format.json { render :show, status: :created, location: place_path(@place.id) }
-      else
-        format.html { render :new }
-        format.json { render json: @place.errors, status: :unprocessable_entity }
-      end
+    if @place.save
+      redirect_to place_path(@place), notice: 'VisitedPlace was successfully created.'
+    else
+      render :new
     end
   end
 
