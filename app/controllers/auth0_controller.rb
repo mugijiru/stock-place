984dc70912dcb4201e585e8562c6648209dded23
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-03-15 18:52:02 +0900

    試してみたらログインできなかったので修正

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,11 +1,13 @@
 class Auth0Controller < ApplicationController
-def callback
+  skip_before_action :logged_in_using_omniauth?
+
+  def callback
     # This stores all the user information that came from Auth0
     # and the IdP
     session[:userinfo] = request.env['omniauth.auth']
 
     # Redirect to the URL you want after successful auth
-    redirect_to '/dashboard'
+    redirect_to '/home'
   end
 
   def failure
