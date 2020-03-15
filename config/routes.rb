f1d6ef30ce179550b9483562f5f3e47e927b90a7
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-03-15 17:54:50 +0900

    Home にアクセスする手段がなくなったので route に追加

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,7 @@ Rails.application.routes.draw do
   get 'auth/auth0/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
 
+  get 'home' => 'home#index'
   resources :stocks, only: %i[index new create destroy]
   resources :visited_places, only: %i[show edit update] do
     resources :reports, only: %i[new create edit update destroy], controller: 'visited_places/reports'
