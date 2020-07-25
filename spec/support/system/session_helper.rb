e9fa0e4f5981eed1789d44e3568982bb0ea3022b
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-07-25 17:35:15 +0900

    system spec 用の auth0 の helper を用意した

---
+++ spec/support/system/session_helper.rb
@@ -0,0 +1,20 @@
+module System
+  module SessionHelpers
+    def log_in(invalid = false)
+      invalid ? mock_invalid_auth_hash : mock_valid_auth_hash
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:auth0]
+      visit '/auth/auth0/callback?code=vihipkGaumc5IVgs'
+    end
+
+    private
+
+    def mock_valid_auth_hash
+      opts = { "provider": 'auth0', "uid": '12345' }
+      OmniAuth.config.mock_auth[:auth0] = OmniAuth::AuthHash.new(opts)
+    end
+
+    def mock_invalid_auth_hash
+      OmniAuth.config.mock_auth[:auth0] = :invalid_credentials
+    end
+  end
+end
