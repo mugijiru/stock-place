7a61e48152099f851bf9ecfefaa335cf6f9d3576
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-07-25 17:51:24 +0900

    request spec 用 helper を request フォルダに閉じ込めた

---
+++ spec/support/request/signin_helper.rb
@@ -0,0 +1,8 @@
+module Request
+  module SigninHelper
+    def signin_as_user
+      allow_any_instance_of(ActionDispatch::Request).to receive(:session).
+        and_return(userinfo: { provider: 'auth0', info: { email: 'foo@example.com' }})
+    end
+  end
+end
--- spec/support/signin_helper.rb
+++
@@ -1,6 +0,0 @@
-module SigninHelper
-  def signin_as_user
-    allow_any_instance_of(ActionDispatch::Request).to receive(:session).
-      and_return(userinfo: { provider: 'auth0', info: { email: 'foo@example.com' }})
-  end
-end
